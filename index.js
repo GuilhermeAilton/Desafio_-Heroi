b4af35e4a4d40b6b10f31de1c87b682d5c681f1d
GuilhermeAilton <145512980+GuilhermeAilton@users.noreply.github.com>
2024-10-11 14:54:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
+const nome = 'Guilherme';
+const xp = 4500; // Substitua por um valor de XP válido
+let nivel;
 
-function determinarNivel(nome, xp) {
-    let nivel;
-    if (xp < 1000) {
-        nivel = "Ferro";
-    } else if (xp >= 1001 && xp <= 2000) {
-        nivel = "Bronze";
-    } else if (xp >= 2001 && xp <= 5000) {
-        nivel = "Prata";
-    } else if (xp >= 5001 && xp <= 7000) {
-        nivel = "Ouro";
-    } else if (xp >= 7001 && xp <= 8000) {
-        nivel = "Platina";
-    } else if (xp >= 8001 && xp <= 9000) {
-        nivel = "Ascendente";
-    } else if (xp >= 9001 && xp <= 10000) {
-        nivel = "Imortal";
-    } else { // xp >= 10001
-        nivel = "Radiante";
-    }
-
-    console.log(`O Herói de nome ${nome} está no nível de ${nivel}.`);
+if (xp < 1000) {
+    nivel = "Ferro";
+} else if (xp < 2001) {
+    nivel = "Bronze";
+} else if (xp < 5001) {
+    nivel = "Prata";
+} else if (xp < 7001) {
+    nivel = "Ouro";
+} else if (xp < 8001) {
+    nivel = "Platina";
+} else if (xp < 9001) {
+    nivel = "Ascendente";
+} else if (xp < 10001) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
 }
 
-const nome = 'guilherme';
-const xp = parseInt(4)
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}.`);
 
-determinarNivel(nome, xp);
+
